4b47969c0f179f3e194214348e676faebd9ea9cf
hendisantika <hendisantika@yahoo.co.id>
2025-04-18 10:37:18 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/customer/CustomerWebApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerWebApplication.class, args);
+    }
+
+}
