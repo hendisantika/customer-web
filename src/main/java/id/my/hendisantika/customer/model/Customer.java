4791fec52ffdaa1a4977dccb853a62c135d5800a
hendisantika <hendisantika@yahoo.co.id>
2025-04-18 10:51:19 +0700

    feat: Add Customer Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/customer/model/Customer.java
@@ -0,0 +1,46 @@
+package id.my.hendisantika.customer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : customer-web
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 18/04/25
+ * Time: 10.48
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
